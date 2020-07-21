ca0351dfaea58c8775eb156c8bd321e3186bfb32
Syed <Syed@LAPTOP-LAHT2RGH>
2020-07-21 15:18:18 +0800

    Did loanchromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -213,6 +213,7 @@ public class ResourceCentre {
 	
 	public static boolean doLoanChromebook(ArrayList<Chromebook> chromebookList, String tag, String dueDate) {
 		// write your code here
+		
 		return true;
 	}
 	public static void loanChromebook(ArrayList<Chromebook> chromebookList) {
