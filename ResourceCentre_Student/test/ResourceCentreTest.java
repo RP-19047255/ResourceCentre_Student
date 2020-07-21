94c173378e41045fe812b88b622300a2ac0d6865
19047287 <19047287@DESKTOP-436HIF5>
2020-07-21 14:53:19 +0800

    succeed

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -47,12 +47,29 @@ public class ResourceCentreTest {
 		//Add another item. test The size of the list is 2?
 		ResourceCentre.addCamcorder(camcorderList, cc2);
 		assertEquals("Test that Camcorder arraylist size is 2?", 2, camcorderList.size());
+		
+		
 	}
 	@Test
 	public void addChromebookTest() {
-		//fail("Not yet implemented");
 		// write your code here
+		
+		// Item list is not null, so that can add a new item
+		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
+		
+		//Given an empty list, after adding 1 item, the size of the list is 1
+		ResourceCentre.addChromebook(chromebookList, cb1);	
+		assertEquals("Test if that Chromebook arraylist size is 1?", 1, chromebookList.size());
+		
+		//The item just added is as same as the first item of the list
+		assertSame("Test that Chromebook is added same as 1st item of the list?", cb1, chromebookList.get(0));
+		
+		//Add another item. test The size of the list is 2?
+		ResourceCentre.addChromebook(chromebookList, cb2);
+
+		
 	}
+	
 	
 	@Test
 	public void retrieveAllCamcorderTest() {
@@ -89,12 +106,25 @@ public class ResourceCentreTest {
 		//fail("Not yet implemented");
 		// write your code here
 		
+
 	}
 	
 	@Test
 	public void doLoanChromebookTest() {
-		//fail("Not yet implemented");
-		// write your code here
+		
+		  // fail("Not yet implemented");
+		  // write your code here
+		  // Test if Item list is not null but empty, so that can loan item
+		  assertNotNull("Test if there is valid Chromebook arraylist to loan", chromebookList);
+
+		  ResourceCentre.doLoanChromebook(chromebookList, cb1.getAssetTag(), cb1.getDueDate());
+
+		  // Test if item is been loaned
+		  assertTrue("Test if item is been loaned", cb1.getIsAvailable());
+
+		  // Test if item is not been loaned
+		  assertFalse("Test if item is been loaned", cb1.getIsAvailable());
+
 	}
 	
 	@Test
