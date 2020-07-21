c75a029eb4cdd4fbf59d70ea1799d3af29baadb6
19005409 <19005409@DESKTOP-DMV0GKL>
2020-07-21 12:57:00 +0800

    add

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -178,6 +178,7 @@ public class ResourceCentre {
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
 		// write your code here
+		String str = "abc";
 	}
 	
 	//================================= Option 3 Loan =================================
